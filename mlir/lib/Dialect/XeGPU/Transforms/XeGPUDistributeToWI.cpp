6d79e8c9e4e9fd4355b5fb1bd0b4abf1ab14023f
Petr Kurapov <petr.a.kurapov@intel.com>
2024-09-23 09:43:16 +0000

    checkpoint

--- mlir/lib/Dialect/XeGPU/Transforms/XeGPUDistributeToWI.cpp
+++ mlir/lib/Dialect/XeGPU/Transforms/XeGPUDistributeToWI.cpp
@@ -32,6 +32,46 @@ struct LoadDistributor final : public OpRewritePattern<xegpu::LoadNdOp> {
   LogicalResult matchAndRewrite(xegpu::LoadNdOp loadOp,
                                 PatternRewriter &rewriter) const override;
 };
+struct StoreDistributor final : public OpRewritePattern<xegpu::StoreNdOp> {
+  using OpRewritePattern<xegpu::StoreNdOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(xegpu::StoreNdOp storeOp,
+                                PatternRewriter &rewriter) const override;
+};
+
+// TODO: smth like this
+// template <class T>
+// llvm::FailureOr<llvm::SmallVector<int64_t>>
+// get_distributed_shape(T op, PatternRewriter &rewriter,
+//                       ArrayRef<int64_t> shape) {
+//   auto origType = op.getTensorDescType();
+//   xegpu::SGMapAttr sgMap = origType.getSGMapAttr();
+//   if (!sgMap) {
+//     rewriter.notifyMatchFailure(
+//         op, "the source tensor descriptor lacks sg_map attribute");
+//     return failure();
+//   }
+
+//   auto origShape = origType.getShape();
+//   if (origShape.size() != 2) {
+//     rewriter.notifyMatchFailure(op, "unsupported shape");
+//     return failure();
+//   }
+
+//   llvm::SmallVector<int64_t> distributedShape;
+//   auto layout = sgMap.getWiLayout();
+
+//   for (const auto [l, o] : llvm::zip(layout, shape)) {
+//     if (!divisible(APInt(64, o), APInt(64, l))) {
+//       rewriter.notifyMatchFailure(
+//           op,
+//           "the tensor dimentions are not divisible by the distribution
+//           factor");
+//       return failure();
+//     }
+//     distributedShape.push_back(o / l);
+//   }
+//   return distributedShape;
+// }
 } // namespace
 
 LogicalResult
@@ -47,7 +87,7 @@ LoadDistributor::matchAndRewrite(xegpu::LoadNdOp loadOp,
   if (origShape.size() != 2)
     return rewriter.notifyMatchFailure(loadOp, "unsupported shape");
 
-  llvm::SmallVector<int64_t, 2> distributedShape, distributedResultShape;
+  llvm::SmallVector<int64_t, 2> distributedResultShape;
   auto layout = sgMap.getWiLayout();
   auto outputVectorShape = loadOp.getType().getShape();
 
@@ -60,29 +100,61 @@ LoadDistributor::matchAndRewrite(xegpu::LoadNdOp loadOp,
       return rewriter.notifyMatchFailure(
           loadOp, "the output vector dimentions are not divisible by the "
                   "distribution factor");
-    distributedShape.push_back(o / l);
     distributedResultShape.push_back(v / l);
   }
 
-  auto distributedType = xegpu::TensorDescType::get(
-      rewriter.getContext(), distributedShape, origType.getElementType(),
-      origType.getEncoding(), {/*removing the sg_map attribute*/});
+  if (loadOp.getPacked())
+    distributedResultShape.push_back(outputVectorShape.back());
 
   auto distributedVectorType = mlir::VectorType::get(
       distributedResultShape, loadOp.getType().getElementType(),
       loadOp.getType().getScalableDims());
 
-  //   static void build(::mlir::OpBuilder &odsBuilder, ::mlir::OperationState
-  //   &odsState, ::mlir::Type value, ::mlir::Value TensorDesc,
-  //   /*optional*/::mlir::UnitAttr packed,
-  //   /*optional*/::mlir::DenseI64ArrayAttr transpose,
-  //   /*optional*/::mlir::xegpu::CachePolicyAttr l1_hint,
-  //   /*optional*/::mlir::xegpu::CachePolicyAttr l2_hint,
-  //   /*optional*/::mlir::xegpu::CachePolicyAttr l3_hint);
   rewriter.replaceOpWithNewOp<xegpu::LoadNdOp>(
       loadOp, distributedVectorType, loadOp.getTensorDesc(),
       loadOp.getPackedAttr(), loadOp.getTransposeAttr(), loadOp.getL1HintAttr(),
       loadOp.getL2HintAttr(), loadOp.getL3HintAttr());
+  return success();
+}
+
+LogicalResult
+StoreDistributor::matchAndRewrite(xegpu::StoreNdOp storeOp,
+                                  PatternRewriter &rewriter) const {
+  auto origType = storeOp.getTensorDescType();
+  xegpu::SGMapAttr sgMap = origType.getSGMapAttr();
+  if (!sgMap)
+    return rewriter.notifyMatchFailure(
+        storeOp, "the source tensor descriptor lacks sg_map attribute");
+
+  auto origShape = origType.getShape();
+  if (origShape.size() != 2)
+    return rewriter.notifyMatchFailure(storeOp, "unsupported shape");
+
+  llvm::SmallVector<int64_t, 2> distributedShape;
+  auto layout = sgMap.getWiLayout();
+  auto inputVectorShape = storeOp.getValueType().getShape();
+
+  for (const auto [l, o] : llvm::zip(layout, inputVectorShape)) {
+    if (!divisible(APInt(64, o), APInt(64, l)))
+      return rewriter.notifyMatchFailure(
+          storeOp,
+          "the tensor dimentions are not divisible by the distribution factor");
+    distributedShape.push_back(o / l);
+  }
+
+  auto storeValue = storeOp.getValue();
+  auto newVectorType = mlir::VectorType::get(
+      distributedShape, storeOp.getValueType().getElementType(),
+      storeOp.getValueType().getScalableDims());
+  storeValue.setType(newVectorType);
+
+  // ::mlir::Value value, ::mlir::Value TensorDesc,
+  // /*optional*/::mlir::xegpu::CachePolicyAttr l1_hint,
+  // /*optional*/::mlir::xegpu::CachePolicyAttr l2_hint,
+  // /*optional*/::mlir::xegpu::CachePolicyAttr l3_hint
+  rewriter.replaceOpWithNewOp<xegpu::StoreNdOp>(
+      storeOp, storeValue, storeOp.getTensorDesc(), storeOp.getL1HintAttr(),
+      storeOp.getL2HintAttr(), storeOp.getL3HintAttr());
   return success();
 }
 
